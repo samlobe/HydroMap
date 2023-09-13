ad2c59c4cff8e40d9c5ab2a41135e08067c43c31
Samuel Lobo <lobo@ucsb.edu>
2023-09-12 21:46:05 -0700

    added error checking to full_hydrophobicity_procedure

--- full_hydrophobicity_procedure.sh
+++ full_hydrophobicity_procedure.sh
@@ -9,7 +9,6 @@
 # #SBATCH --mail-user=<yourEmail> # uncomment these two lines and include email if desired
 # #SBATCH --mail-type=END,FAIL    
 
-
 # Check if a protein name was provided
 if [ -z "$1" ]; then
     echo "Usage: $0 <protein_name>"
@@ -20,12 +19,24 @@ PROTEIN_NAME="$1"
 
 # Step 1: Process with GROMACS
 bash process_with_gromacs "${PROTEIN_NAME}.pdb"
+if [ $? -ne 0 ]; then
+    echo "Error processing with GROMACS. Exiting."
+    exit 1
+fi
 
 # Step 2: Submit the simulation and capture the job ID
 JOB_ID_1=$(python submit_simulation.py "${PROTEIN_NAME}_processed.gro")
+if [ $? -ne 0 ]; then
+    echo "Error submitting simulation. Exiting."
+    exit 1
+fi
 
 # Step 3: Submit the triplets (with dependency on the previous job) and capture job IDs
-JOB_ID_2_ARRAY=($(cd water_triplets && python submit_triplets.py "${PROTEIN_NAME}.pdb" --dependency=afterany:$JOB_ID_1))
+JOB_ID_2_ARRAY=($(cd water_triplets && python submit_triplets.py "../${PROTEIN_NAME}.pdb" ../traj.dcd --dependency=afterany:$JOB_ID_1))
+if [ $? -ne 0 ]; then
+    echo "Error submitting triplet analysis. Exiting."
+    exit 1
+fi
 
 # Wait until EACH job from Step 3 is finished
 for job in "${JOB_ID_2_ARRAY[@]}"; do
@@ -35,5 +46,14 @@ for job in "${JOB_ID_2_ARRAY[@]}"; do
 done
 
 # Step 4: Directly run the Python scripts without sbatch
-python process_angles.py "${PROTEIN_NAME}.pdb"
-python analyze_groups.py "${PROTEIN_NAME}.pdb"
+python process_angles.py "../${PROTEIN_NAME}.pdb"
+if [ $? -ne 0 ]; then
+    echo "Error processing triplet angles. Exiting."
+    exit 1
+fi
+
+python analyze_groups.py "../${PROTEIN_NAME}.pdb"
+if [ $? -ne 0 ]; then
+    echo "Error analyzing triplet angles or outputting pdbs. Exiting."
+    exit 1
+fi
