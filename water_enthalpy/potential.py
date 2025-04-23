streamlining procedure. fixed some missing flags. made water normalization for potential as default. enabled skipping frames in triplet analysis

water_enthalpy/potential.py
@@ -91,10 +91,8 @@ def main():
                         help="Frame stride (default 50; reduce for better confidence bars)")
     parser.add_argument("--nogpu", action="store_true",
                         help="Force CPU platform")
-    parser.add_argument("--cutoff", type=float, default=0.55,
-                        help="Cutoff distance in nm (default 0.55)")
-    parser.add_argument("--norm_per_water", action="store_true",
-                        help="Normalize per water residues in cutoff")
+    parser.add_argument("--cutoff", type=float, default=5.5,
+                        help="Cutoff distance in Angstroms (default 5.5)")
     parser.add_argument("--outdir", type=str, default="energies",
                         help="Output directory (default: energies)")
     args = parser.parse_args()
@@ -152,7 +150,7 @@ def main():
     
     top = GromacsTopFile(args.top,
                          periodicBoxVectors=box_vectors)
-    system = prepare_system(top, pdb, group_idx, solvent_idx, cutoff_nm=args.cutoff) # pdb instead of gro for pdb support laters
+    system = prepare_system(top, pdb, group_idx, solvent_idx, cutoff_nm=args.cutoff/10) # pdb instead of gro for pdb support laters
 
     # pick platform
     if args.nogpu:
@@ -172,32 +170,31 @@ def main():
     frames_to_analyse = int(args.time*1000 / u.trajectory.dt)
     first = max(0, nframes - frames_to_analyse)
 
-    coul_list, lj_list = [], []
+    coul_list, lj_list, nwaters_list = [], [], []
 
     for ts in tqdm(u.trajectory[first::args.skip]):
         positions_nm = ts.positions / 10.0  # Å → nm
         context.setPositions(positions_nm * nanometer)
 
-        if args.norm_per_water:
-            water_oxygens = 'resname SOL and name O*'
-            group = sel_str
-            shell_waters = u.select_atoms(f'{water_oxygens} and around {args.cutoff * 10} ({group})')
-            n_within_cutoff = len(shell_waters)
-            if n_within_cutoff == 0:
-                n_within_cutoff = 1 # avoid division by zero
+        water_oxygens = 'resname SOL and name O*'
+        group = sel_str
+        shell_waters = u.select_atoms(f'{water_oxygens} and around {args.cutoff} ({group})')
+        n_within_cutoff = len(shell_waters)
 
         total    = coulomb_energy(context, 1, 1)
         group_ce = coulomb_energy(context, 1, 0)
         solv_ce  = coulomb_energy(context, 0, 1)
         coul     = (total - group_ce - solv_ce).value_in_unit(kilojoule_per_mole)
         lj = context.getState(getEnergy=True, groups={1}).getPotentialEnergy()
-        
-        if args.norm_per_water:
+
+        # normalize per-water   
+        if n_within_cutoff > 0:     
             lj = lj / n_within_cutoff
             coul = coul / n_within_cutoff
 
         coul_list.append(coul)
         lj_list.append(lj.value_in_unit(kilojoule_per_mole))
+        nwaters_list.append(n_within_cutoff)
 
     total_arr = np.array(coul_list) + np.array(lj_list)
 
@@ -223,7 +220,8 @@ def main():
     df = pd.DataFrame({
         "coulomb": coul_list,
         "lj":      lj_list,
-        "total":   total_arr
+        "total":   total_arr,
+        "n_waters": nwaters_list
     })
     df.to_csv(out_csv, index=False)
     print(f"\nSaved energies from {len(df)} frames of the MD simulation to {out_csv}")

water_enthalpy/process_potentials.py
@@ -24,6 +24,7 @@ from tqdm import tqdm
 # ----------------------------------------------------------------------
 parser = argparse.ArgumentParser(description="Process potentials and output group averages.")
 parser.add_argument("protein", help="Unprocessed protein file (e.g. myProtein.pdb)")
+parser.add_argument("--energiesDir", type=str, default="energies", help="Directory containing the energies files")
 parser.add_argument("--multiChain", action="store_true", help="Protein has multiple chains")
 parser.add_argument("--groupsFile", type=str, help="File listing custom groups (MDAnalysis selection strings)")
 parser.add_argument("--onePotentialsFile", type=str, help="Process just a single potentials CSV file")
@@ -112,7 +113,7 @@ elif args.groupsFile:
         groups_selection = [line for line in f if not line.strip().startswith('#')]
     total_groups = len(groups_selection)
     for group_num in tqdm(np.arange(1, total_groups+1)):
-        potentials_path = f'{script_dir}/energies/{protein_name}_group{group_num}_energies.csv'
+        potentials_path = f'{script_dir}/{args.energiesDir}/{protein_name}_group{group_num}_energies.csv'
         if not os.path.exists(potentials_path):
             print(f"Group {group_num} data missing: {potentials_path}")
             continue
@@ -130,14 +131,14 @@ else:
     segids = u.residues.segids
     for resid, segid in zip(tqdm(resids), segids):
         if args.multiChain:
-            potentials_path = f'{script_dir}/energies/{protein_name}_res{resid}_chain{segid}_energies.csv'
+            potentials_path = f'{script_dir}/{args.energiesDir}/{protein_name}_res{resid}_chain{segid}_energies.csv'
             if not os.path.exists(potentials_path):
                 print(f"Data missing for: residue {resid} chain {segid}")
                 continue
             seq_id = resid - u.residues.resids[0]
             group_names.append(f'{sequence[seq_id]}{resid}_chain{segid}')
         else:
-            potentials_path = f'{script_dir}/energies/{protein_name}_res{resid}_energies.csv'
+            potentials_path = f'{script_dir}/{args.energiesDir}/{protein_name}_res{resid}_energies.csv'
             if not os.path.exists(potentials_path):
                 print(f"Data missing for: residue {resid}")
                 continue
@@ -148,12 +149,13 @@ else:
         avg_coulomb = df['coulomb'].mean()
         avg_lj = df['lj'].mean()
         avg_total = df['total'].mean()
-        group_means.append((avg_coulomb, avg_lj, avg_total))
+        avg_nwaters = df['n_waters'].mean()
+        group_means.append((avg_coulomb, avg_lj, avg_total, avg_nwaters))
 
 # ----------------------------------------------------------------------
 #  Assemble dataframe
 # ----------------------------------------------------------------------
-columns = ['avg_coulomb (kJ/mol)', 'avg_lj (kJ/mol)', 'avg_total (kJ/mol)']
+columns = ['avg_coulomb (kJ/mol)', 'avg_lj (kJ/mol)', 'avg_total (kJ/mol)', 'avg_n_waters']
 groups_df = pd.DataFrame(data=group_means, index=group_names, columns=columns)
 
 output_filename = args.outputFile if args.outputFile else f'{protein_name}_potential_data.csv'

water_triplets/process_PCs.py
@@ -12,7 +12,8 @@ import MDAnalysis as mda
 parser = argparse.ArgumentParser(description="Generate dewetting free energy predictions and PC contributions from the triplet distributions, and output 'colored' pdb.\nExample usage: `python analyze_groups.py ../myProtein_withH.pdb`")
 # Add arguments
 parser.add_argument('protein', help="unprocessed protein pdb file to color, e.g. myProtein_withH.pdb\n(recommended to use a pdb of protein without solvent or ions, and with hydrogens present)")
-
+parser.add_argument('--tripletFile', type=str, help="CSV file containing triplet data; will default to <protein_name>_triplet_data.csv")
+parser.add_argument('--PCs_outputFile', type=str, help="CSV file to save the PCs to; will default to <protein_name>_PCs.csv")
 args = parser.parse_args()
 
 # Assign arguments
@@ -37,14 +38,25 @@ if '/' in protein_name:
     protein_name = protein_name.split('/')[-1]
 
 # Find the data file with the triplet angles
-groups_df = pd.read_csv(f'{protein_name}_triplet_data.csv',index_col=0)
+if args.tripletFile: 
+    triplet_file = args.tripletFile
+else: 
+    triplet_file = f'{protein_name}_triplet_data.csv'
+groups_df = pd.read_csv(triplet_file,index_col=0)
+
+PCs_df = convert_triplets.get_PCs(groups_df,'a99SBdisp')
+
+if args.PCs_outputFile:
+    PCs_df.to_csv(args.PCs_outputFile)
+else:
+    PCs_df.to_csv(f'{protein_name}_PCs.csv')
 
 #%%
 # convert triplet distribution to predicted dewetting free energy
 # using the model from fitting to single amino acids
-dewet_df = convert_triplets.singleAA_dewet_fit(groups_df,'a99SBdisp')
+# dewet_df = convert_triplets.singleAA_dewet_fit(groups_df,'a99SBdisp')
 # save the df
-dewet_df.to_csv(f'{protein_name}_Fdewet.csv')
+# dewet_df.to_csv(f'{protein_name}_Fdewet.csv')
 # the columns are 'FDewet (kJ/mol)' and 'MDAnalysis_selection_string'
 
 ## modelling hydrophobicity in a03ws and CHARMM36m:
@@ -60,9 +72,9 @@ dewet_df.to_csv(f'{protein_name}_Fdewet.csv')
 # convert triplet distribution to PC1, PC2, and PC3 contributions
 # using Robinson / Jiao's PCs from their triplet distributions
 # (they subtracted out the bulk water triplet distribution before getting PCs)
-PCs_df = convert_triplets.get_PCs(groups_df,'a99SBdisp')
-# save the df
-PCs_df.to_csv(f'{protein_name}_PCs.csv')
+# PCs_df = convert_triplets.get_PCs(groups_df,'a99SBdisp')
+# # save the df
+# PCs_df.to_csv(f'{protein_name}_PCs.csv')
 ## the columns are 'PC1','PC2','PC3', and 'MDAnalysis_selection_string'
 
 ## if using other force fields:
@@ -77,76 +89,76 @@ PCs_df.to_csv(f'{protein_name}_PCs.csv')
 #%%
 # function to color the protein based on a property (e.g. dewetting free energy or a PC)
 # (actually the tempfactor for each atom is set, and later we will use ChimeraX or nglview to color the protein)
-def color_pdb(pdb_path, df_wProp_selecStr, property):
-    protein_name = pdb_path[:-4]
-    # check if the protein name has '/' in it (e.g. '../myProtein')
-    # if so read the part after the last '/' (e.g. 'myProtein')
-    if '/' in protein_name:
-       protein_name = protein_name.split('/')[-1] 
+# def color_pdb(pdb_path, df_wProp_selecStr, property):
+#     protein_name = pdb_path[:-4]
+#     # check if the protein name has '/' in it (e.g. '../myProtein')
+#     # if so read the part after the last '/' (e.g. 'myProtein')
+#     if '/' in protein_name:
+#        protein_name = protein_name.split('/')[-1] 
 
-    # load the structure
-    u = mda.Universe(pdb_path)
-    # initialize all tempfactors to -100
-    for atom in u.atoms:
-        atom.tempfactor = -100
-    # loop through the df and assign the property to the groups' atoms' tempfactors
-    for group in df_wProp_selecStr.index:
-        selection_string = df_wProp_selecStr['MDAnalysis_selection_strings'][group]
-        for atom in u.select_atoms(selection_string):
-            atom.tempfactor = np.around(df_wProp_selecStr[property][group],2)
-    # save the structure
-    u.atoms.write(f'{protein_name}_{property}_colored.pdb')
-    print(f'Outputted {protein_name}_{property}_colored.pdb')
-    # return the MDAnalysis universe object
-    return u
+#     # load the structure
+#     u = mda.Universe(pdb_path)
+#     # initialize all tempfactors to -100
+#     for atom in u.atoms:
+#         atom.tempfactor = -100
+#     # loop through the df and assign the property to the groups' atoms' tempfactors
+#     for group in df_wProp_selecStr.index:
+#         selection_string = df_wProp_selecStr['MDAnalysis_selection_strings'][group]
+#         for atom in u.select_atoms(selection_string):
+#             atom.tempfactor = np.around(df_wProp_selecStr[property][group],2)
+#     # save the structure
+#     u.atoms.write(f'{protein_name}_{property}_colored.pdb')
+#     print(f'Outputted {protein_name}_{property}_colored.pdb')
+#     # return the MDAnalysis universe object
+#     return u
 
-u_Fdewet = color_pdb(pdb_path, dewet_df, 'Fdewet')
-u_PC1 =    color_pdb(pdb_path, PCs_df, 'PC1')
-u_PC2 =    color_pdb(pdb_path, PCs_df, 'PC2')
-u_PC3 =    color_pdb(pdb_path, PCs_df, 'PC3')
+# u_Fdewet = color_pdb(pdb_path, dewet_df, 'Fdewet')
+# u_PC1 =    color_pdb(pdb_path, PCs_df, 'PC1')
+# u_PC2 =    color_pdb(pdb_path, PCs_df, 'PC2')
+# u_PC3 =    color_pdb(pdb_path, PCs_df, 'PC3')
 
-#%% 
-# plot histograms of Fdewet, PC1, PC2, and PC3 in 2x2 grid
-fig, axes = plt.subplots(2,2,figsize=(7,7))
-axes[0,0].hist(dewet_df['Fdewet'],bins=20)
-axes[0,0].set_xlabel('Dewetting free energy (kJ/mol)',fontsize=14)
-axes[0,0].set_ylabel('Number of groups',fontsize=14)
-axes[0,1].hist(PCs_df['PC1'],bins=20)
-axes[0,1].set_xlabel('PC1',fontsize=14)
-axes[0,1].set_ylabel('Number of groups',fontsize=14)
-axes[1,0].hist(PCs_df['PC2'],bins=20)
-axes[1,0].set_xlabel('PC2',fontsize=14)
-axes[1,0].set_ylabel('Number of groups',fontsize=14)
-axes[1,1].hist(PCs_df['PC3'],bins=20)
-axes[1,1].set_xlabel('PC3',fontsize=14)
-axes[1,1].set_ylabel('Number of groups',fontsize=14)
-# make sure there aren't decimals in the y-ticks
-for ax in axes.flatten():
-    ax.yaxis.set_major_locator(plt.MaxNLocator(integer=True))
-plt.tight_layout()
-plt.savefig(f'{protein_name}_histograms.png')
-print(f'Outputted {protein_name}_histograms.png')
-# plt.show() # uncomment if running this on your personal computer
-#%%
-# plot heatmaps of PC1 vs PC2, PC1 vs PC3, and PC2 vs PC3 in 1x3 grid
-fig, axes = plt.subplots(1,3,figsize=(15,5))
-axes[0].scatter(PCs_df['PC1'],PCs_df['PC2'])
-axes[0].set_xlabel('PC1',fontsize=14)
-axes[0].set_ylabel('PC2',fontsize=14)
-axes[1].scatter(PCs_df['PC1'],PCs_df['PC3'])
-axes[1].set_xlabel('PC1',fontsize=14)
-axes[1].set_ylabel('PC3',fontsize=14)
-axes[2].scatter(PCs_df['PC2'],PCs_df['PC3'])
-axes[2].set_xlabel('PC2',fontsize=14)
-axes[2].set_ylabel('PC3',fontsize=14)
-# label each point with the residue name (from groups_df.index)
-for i,res in enumerate(PCs_df.index):
-    axes[0].annotate(res,(PCs_df['PC1'].iloc[i],PCs_df['PC2'].iloc[i]))
-    axes[1].annotate(res,(PCs_df['PC1'].iloc[i],PCs_df['PC3'].iloc[i]))
-    axes[2].annotate(res,(PCs_df['PC2'].iloc[i],PCs_df['PC3'].iloc[i]))
-plt.tight_layout()
-plt.savefig(f'{protein_name}_PCs_2D.png')
-print(f'Outputted {protein_name}_PCs_2D.png')
+# #%% 
+# # plot histograms of Fdewet, PC1, PC2, and PC3 in 2x2 grid
+# fig, axes = plt.subplots(2,2,figsize=(7,7))
+# axes[0,0].hist(dewet_df['Fdewet'],bins=20)
+# axes[0,0].set_xlabel('Dewetting free energy (kJ/mol)',fontsize=14)
+# axes[0,0].set_ylabel('Number of groups',fontsize=14)
+# axes[0,1].hist(PCs_df['PC1'],bins=20)
+# axes[0,1].set_xlabel('PC1',fontsize=14)
+# axes[0,1].set_ylabel('Number of groups',fontsize=14)
+# axes[1,0].hist(PCs_df['PC2'],bins=20)
+# axes[1,0].set_xlabel('PC2',fontsize=14)
+# axes[1,0].set_ylabel('Number of groups',fontsize=14)
+# axes[1,1].hist(PCs_df['PC3'],bins=20)
+# axes[1,1].set_xlabel('PC3',fontsize=14)
+# axes[1,1].set_ylabel('Number of groups',fontsize=14)
+# # make sure there aren't decimals in the y-ticks
+# for ax in axes.flatten():
+#     ax.yaxis.set_major_locator(plt.MaxNLocator(integer=True))
+# plt.tight_layout()
+# plt.savefig(f'{protein_name}_histograms.png')
+# print(f'Outputted {protein_name}_histograms.png')
+# # plt.show() # uncomment if running this on your personal computer
+# #%%
+# # plot heatmaps of PC1 vs PC2, PC1 vs PC3, and PC2 vs PC3 in 1x3 grid
+# fig, axes = plt.subplots(1,3,figsize=(15,5))
+# axes[0].scatter(PCs_df['PC1'],PCs_df['PC2'])
+# axes[0].set_xlabel('PC1',fontsize=14)
+# axes[0].set_ylabel('PC2',fontsize=14)
+# axes[1].scatter(PCs_df['PC1'],PCs_df['PC3'])
+# axes[1].set_xlabel('PC1',fontsize=14)
+# axes[1].set_ylabel('PC3',fontsize=14)
+# axes[2].scatter(PCs_df['PC2'],PCs_df['PC3'])
+# axes[2].set_xlabel('PC2',fontsize=14)
+# axes[2].set_ylabel('PC3',fontsize=14)
+# # label each point with the residue name (from groups_df.index)
+# for i,res in enumerate(PCs_df.index):
+#     axes[0].annotate(res,(PCs_df['PC1'].iloc[i],PCs_df['PC2'].iloc[i]))
+#     axes[1].annotate(res,(PCs_df['PC1'].iloc[i],PCs_df['PC3'].iloc[i]))
+#     axes[2].annotate(res,(PCs_df['PC2'].iloc[i],PCs_df['PC3'].iloc[i]))
+# plt.tight_layout()
+# plt.savefig(f'{protein_name}_PCs_2D.png')
+# print(f'Outputted {protein_name}_PCs_2D.png')
 # plt.show() # uncomment if running this on your personal computer
 #%%
 # now you can use ChimeraX or Pymol (or nglview) to color the protein

water_triplets/process_angles.py
@@ -12,6 +12,7 @@ from tqdm import tqdm
 parser = argparse.ArgumentParser(description='Read the triplet angles of your protein and output triplet distributions.')
 # Add arguments
 parser.add_argument('protein', help="unprocessed protein file ( e.g. <protein>[.pdb] )")
+parser.add_argument('--anglesDir', type=str, default='angles', help="directory containing the angles files")
 parser.add_argument('--multiChain', action='store_true', help="protein has multiple chains")
 parser.add_argument('--groupsFile', type=str, help='to reference a file containing your custom groups (i.e. MDAnalysis selection strings, one group per line).')
 parser.add_argument('--oneAnglesFile', type=str, help='to process data from one angles file (e.g. angles/<protein>_<selection>_angles.txt)')
@@ -143,7 +144,7 @@ elif args.groupsFile: # reading the custom groups in the groups file if given
         groups_selection = [line for line in f if not line.strip().startswith('#')]
     total_groups = len(groups_selection)
     for group_num in tqdm(np.arange(1,total_groups+1)):
-        group_file = f'{script_dir}/angles/{protein_name}_group{group_num}_angles.txt'
+        group_file = f'{script_dir}/{args.anglesDir}/{protein_name}_group{group_num}_angles.txt'
         if not os.path.exists(group_file):
             print(f'Group {group_num} data is missing.')
             continue
@@ -164,7 +165,7 @@ else:
     groups_selection = [] # for MDAnalysis selection strings
     for i, (resid, segid) in enumerate(zip(tqdm(resids), segids)):
         if args.multiChain:
-            group_file = f'{script_dir}/angles/{protein_name}_res{resid}_chain{segid}_angles.txt'
+            group_file = f'{script_dir}/{args.anglesDir}/{protein_name}_res{resid}_chain{segid}_angles.txt'
             if not os.path.exists(group_file):
                 print(f'Data missing for: residue {resid} chain {segid}')
                 continue
@@ -172,7 +173,7 @@ else:
             group_names.append(f'{sequence[seq_id]}{resid}_chain{segid}')
             groups_selection.append(f'resid {resid} and segid {segid}') # not excluding hydrogens
         else:
-            group_file = f'{script_dir}/angles/{protein_name}_res{resid}_angles.txt'
+            group_file = f'{script_dir}/{args.anglesDir}/{protein_name}_res{resid}_angles.txt'
             if not os.path.exists(group_file):
                 print(f'Data missing for: residue {resid}')
                 continue